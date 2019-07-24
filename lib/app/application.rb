aaa51f6555894f5b05c7c689dbabccf515738506
Carsano <damien.fabiano@gadz.org>
2019-07-24 10:26:27 +0200

    add def new_board in application

--- lib/app/application.rb
+++ lib/app/application.rb
@@ -8,8 +8,11 @@ class Application
   def ask_name
     puts 'Veuillez rentrer le nom du joueur1'
     @joueur1 = gets.chomp
-puts 'Veuillez rentrer le nom du joueur2'
+    puts 'Veuillez rentrer le nom du joueur2'
     @joueur2 = gets.chomp
+  end
 
+  def new_board
+    @board = Board.new
   end
 end
