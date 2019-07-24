f4b238e5b6629d99ee65ad3995ff3b3178a3988d
Carsano <damien.fabiano@gadz.org>
2019-07-24 08:51:26 +0200

    add Class game and reference in application

---
+++ application.rb
@@ -0,0 +1,9 @@
+require 'bundler'
+Bundler.require
+
+$:.unshift File.expand_path("./../lib", __FILE__)
+require 'app/game'
+require 'app/board'
+require 'app/player'
+require 'app/boardcase'
+require 'views/show'
