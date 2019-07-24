029897c6a588c6f842cc4ea2862c16784ed359d5
Carsano <damien.fabiano@gadz.org>
2019-07-24 08:53:22 +0200

    add def initialize and endgame in game.rb

--- lib/app/game.rb
+++ lib/app/game.rb
@@ -1,3 +1,11 @@
 class Game
 
+  def initialize
+    # initialize une nouvelle partie
+  end
+
+  def verify_endgame
+    # On va vérifier si 3 pions sont alignés
+    # vérifie si le plateau est complet
+  end
 end
