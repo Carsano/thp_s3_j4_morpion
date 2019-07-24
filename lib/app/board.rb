b5044071258ab569bb82df1871abb44c890ce2f9
Carsano <damien.fabiano@gadz.org>
2019-07-24 11:28:33 +0200

    add def verif_alignement_pointsw

--- lib/app/board.rb
+++ lib/app/board.rb
@@ -9,4 +9,17 @@ class Board
   def change_value_case(bcase, value)
     @case[bcase]=value
   end
+
+  def verif_alignement_points
+    # verif lignes
+    if (@case[a1]==@case[a2] and @case[a1] = @case[a3] and @case[a1] != "") or (@case[b1]==@case[b2] and @case[b1] = @case[b3] and @case[b1] != "") and (@case[c1]==@case[c2] and @case[c1] = @case[c3] and @case[c1] != "")
+      return true
+    elsif (@case[a1]==@case[b1] and @case[a1] = @case[c1] and @case[a1] != "") or (@case[a2]==@case[b2] and @case[a2] = @case[c2] and @case[a2] != "") and (@case[a3]==@case[b3] and @case[a3] = @case[c3] and @case[a3] != "")
+      return true
+    elsif (@case[a1]==@case[b2] and @case[a1] = @case[c3] and @case[a1] != "") or (@case[a3]==@case[b2] and @case[a3] = @case[c1] and @case[a3] != "")
+      return true
+    else
+      return false
+    end
+  end
 end
