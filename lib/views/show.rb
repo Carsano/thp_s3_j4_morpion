08dd3125a87384ada60003ffb8f69fe5bde332a2
Carsano <damien.fabiano@gadz.org>
2019-07-24 18:00:10 +0200

    Maj headings

--- lib/views/show.rb
+++ lib/views/show.rb
@@ -6,7 +6,7 @@ attr_reader :table_morp
       ["A", board.cases[0].content, board.cases[1].content, board.cases[2].content],
       ["B", board.cases[3].content, board.cases[4].content, board.cases[5].content],
       ["C", board.cases[6].content, board.cases[7].content, board.cases[8].content] ]
-    @table_morp = Terminal::Table.new :headings => ['1','2','3'],:rows => rows_morp
+    @table_morp = Terminal::Table.new :headings => ['','1','2','3'],:rows => rows_morp
   end
 
 end
