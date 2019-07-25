cd3c8b07ff083f687880a48f616abb755fe5245a
Carsano <damien.fabiano@gadz.org>
2019-07-25 14:06:40 +0200

    add comments in game

--- lib/app/game.rb
+++ lib/app/game.rb
@@ -1,27 +1,26 @@
 class Game
 
   attr_reader :board, :active_player, :j1, :j2
-  def initialize(j1,j2)
+  def initialize(j1,j2, active_player=j1)
     # initialize une nouvelle partie
-    @board = Board.new
-    @cases_availables = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3", "QUITTER LA PARTIE"]
+    @board = Board.new # On crée un nouveau board
+    @cases_availables = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3", "QUITTER LA PARTIE"] # Les cases de choix de jeu
     @prompt = TTY::Prompt.new
     @j1 = j1
     @j2 = j2
-    @active_player = @j1
+    @active_player = active_player
   end
 
   def verify_endgame
-    # On va vérifier si 3 pions sont alignés
+    # On va vérifier si 3 pions sont alignés ou si le jeu est complet, ou si le jeu est quitté en cours
     if @board.verif_alignement_points or @board.nb_coups_joues == 9 or @board.aborted 
       true
     else
       false
     end
-    # vérifie si le plateau est complet
   end
 
-  def place_value
+  def place_value # On place la value
     choice = @prompt.select("#{@active_player.name} (#{@active_player.symbol}), à vous de jouer : ", @cases_availables, cycle: true) 
     @cases_availables.delete(choice)
     @board.change_value_case(transform_choice(choice), @active_player.symbol)
