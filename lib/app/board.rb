23d7da5b1902624a6b997a46e1400915185931df
Carsano <damien.fabiano@gadz.org>
2019-07-25 14:12:23 +0200

    add comments and maj in board.rb

--- lib/app/board.rb
+++ lib/app/board.rb
@@ -3,6 +3,7 @@ class Board
   attr_reader :cases, :nb_coups_joues, :aborted
   def initialize
     # Ici on initialise un plateau
+    # On crée les boardcases
     a1=BoardCase.new(1) 
     a2=BoardCase.new(2)
     a3=BoardCase.new(3) 
@@ -12,39 +13,40 @@ class Board
     c1=BoardCase.new(7) 
     c2=BoardCase.new(8) 
     c3=BoardCase.new(9)  
-    @cases = [a1, a2, a3, b1, b2, b3, c1, c2, c3]
+    @cases = [a1, a2, a3, b1, b2, b3, c1, c2, c3] # On stocke les boardcases
     @nb_coups_joues = 0
     @aborted = false
   end
 
-  def change_value_case(bcase, value)
+  def change_value_case(bcase, value) # On applique le changement à la case
     begin
       @cases[bcase].change_content(value)
       add_coup
     rescue
-      ending_play
+      ending_play # Si on joue quitter
     end
   end
 
   def ending_play
-    @nb_coups_joues = 9
     @aborted = true
   end
+
   def add_coup
     @nb_coups_joues += 1
   end
 
-  def verif_alignement_points
+  def verif_alignement_points # On vérifie si le joueur gagne
     if verif_lines or verif_columns or verif_diagos
       true
     else
       false
     end
   end
-  def verif_lines
+
+  def verif_lines # On vérifie chaque ligne
     # verif lignes
     if @cases[0].content != " " and [@cases[0].content, @cases[1].content, @cases[2].content] == [@cases[0].content, @cases[0].content, @cases[0].content]
-      colorize_win(@cases[0], @cases[1], @cases[2])
+      colorize_win(@cases[0], @cases[1], @cases[2]) # Si c'est bon on color en vert les cases
       return true
     elsif @cases[3].content != " " and [@cases[3].content, @cases[4].content, @cases[5].content] == [@cases[3].content, @cases[3].content, @cases[3].content]
       colorize_win(@cases[3], @cases[4], @cases[5])
@@ -85,7 +87,7 @@ class Board
     end
   end
 
-  def colorize_win(cells1, cells2, cells3)
+  def colorize_win(cells1, cells2, cells3) # Color les cases en vert
     cells1.change_color
     cells2.change_color
     cells3.change_color
