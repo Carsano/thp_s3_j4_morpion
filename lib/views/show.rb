3528c97d3a32926dc15804f079ba6a1be5a86419
Carsano <damien.fabiano@gadz.org>
2019-07-25 14:18:53 +0200

    add comments and refactor in show.rb

--- lib/views/show.rb
+++ lib/views/show.rb
@@ -1,5 +1,4 @@
 class Show
-  attr_reader :table_morp
   def initialize(game)
     @game = game
   end
@@ -8,10 +7,9 @@ class Show
     rows_morp = [
       ["A".colorize(:light_red), @game.board.cases[0].content, @game.board.cases[1].content, @game.board.cases[2].content],
       ["B".colorize(:light_red), @game.board.cases[3].content, @game.board.cases[4].content, @game.board.cases[5].content],
-      ["C".colorize(:light_red), @game.board.cases[6].content, @game.board.cases[7].content, @game.board.cases[8].content] ]
-    @table_morp = Terminal::Table.new :headings => ['','1'.colorize(:light_red),'2'.colorize(:light_red),'3'.colorize(:light_red)],:rows => rows_morp
-    @table_morp.style = {:border_x => "-".colorize(:white),:border_y => "|".colorize(:white),:border_i => "+".colorize(:white), :all_separators => true}
-    @table_morp
+      ["C".colorize(:light_red), @game.board.cases[6].content, @game.board.cases[7].content, @game.board.cases[8].content] 
+    ]
+    Terminal::Table.new :headings => ['','1'.colorize(:light_red),'2'.colorize(:light_red),'3'.colorize(:light_red)],:rows => rows_morp, :style => {:border_x => "-".colorize(:white),:border_y => "|".colorize(:white),:border_i => "+".colorize(:white), :all_separators => true}
   end
 
   def show_stats_players
@@ -21,7 +19,7 @@ class Show
       ["Loss","#{@game.j1.show_states.values[3]}", "#{@game.j2.show_states.values[3]}"],
       ["Even","#{@game.j1.show_states.values[4]}", "#{@game.j2.show_states.values[4]}"]
     ]
-    stats = Terminal::Table.new :title => "Stats", :headings => ['Parameters', "#{@game.j1.name}", "#{@game.j2.name}"], :rows => rows_stats, :style => {:alignment => :center, :width => 40}
+    Terminal::Table.new :title => "Stats", :headings => ['Parameters', "#{@game.j1.name}", "#{@game.j2.name}"], :rows => rows_stats, :style => {:alignment => :center, :width => 40}
   end
 end
 
